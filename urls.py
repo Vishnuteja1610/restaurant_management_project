7e7d52a4685c675b96619749fd38ffad74468710
Nalumachu Vishnu Teja <vishnuteja2583@gmail.com>
2025-08-30 16:38:59 +0530

    urls.py

--- urls.py
+++ urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import menu_api
+from . import views 
+
 
 urlpatterns = [
     path('api/menu/',menu_api, name='menu_api'),
+    path('',views.home, name='home'),
+    path('faq/', views.faq,name='faq'),
 ]
