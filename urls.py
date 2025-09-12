d51e5819ccffff3d3ad1b5e3dd9f3b09cebce335
Nalumachu Vishnu Teja <vishnuteja2583@gmail.com>
2025-09-12 09:30:23 +0530

    43998

--- urls.py
+++ urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("contact/",views.contact_view,name="contact"),
-    path("thank-you/", views.thank_you_view, name="thank_you"),
+    path("about/",views.about_us,name="about"),
 ]
