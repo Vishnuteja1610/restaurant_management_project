5a0f554b952d08da3fab8ab045297e5875cfa560
Nalumachu Vishnu Teja <vishnuteja2583@gmail.com>
2025-09-21 22:38:19 +0530

    49959

--- views.py
+++ views.py
@@ -1,25 +1,9 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from django.core.paginator import Paginator
-from .models import MenuItem
-from .serializers import MenuItemSerializer
+from rest_framework.generics import RetrieveAPIView
+from .models import Order
+from .serializers import OrderSerializer
 
-class MenuItemSearchView(APIView):
-    def get(self,request):
-        query = request.GET.get("q","")
-        items = MenuItem.objects.filter(name__icontains= query)if query else MenuItem.objects.all()
-
-        page_number = request.GET.get("page",1)
-        paginator = paginator(items, 10)
-        page_obj = paginator.get_page(page_number)
-
-        serializer = MenuItemSerializer(page_obj, many=True)
-
-        return Response({
-            "count": paginator.count,
-            "total_pages":paginator.num_pages,
-            "current_page":page_obj.number,
-            "results":serializer.data,
-        } status=status.HTTP_200_OK)
-        
+class OrderRetrieveAPIView(RetrieveAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    lookup_field = 'order_id'
+    
