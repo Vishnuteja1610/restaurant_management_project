be638c7bac8351d4d0623279bd75329d80914ff0
Nalumachu Vishnu Teja <vishnuteja2583@gmail.com>
2025-09-21 23:16:50 +0530

    49962

--- models.py
+++ models.py
@@ -1,9 +1,7 @@
 from django.db import models
 
-class MenuItem(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-
-    def __str__(self):
-        return self.name
+class Resturant(models.Model):
+    opening_days = models.CharField(
+        max_length=50,
+        help_text="Comma-separated days, e.g. 'Mon,Tue,Wed,Thu,Fri'"
+    )
