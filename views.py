508d0f1b6336c992e48d7f188a2bc7706217fc99
Nalumachu Vishnu Teja <vishnuteja2583@gmail.com>
2025-09-21 09:22:58 +0530

    49142

--- views.py
+++ views.py
@@ -1,18 +1,25 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, permissions
-from django.shortcuts import get_object_or_404
+from rest_framework import status
+from django.core.paginator import Paginator
 from .models import MenuItem
 from .serializers import MenuItemSerializer
 
-class UpdateMenuItemView(APIView):
-    permission_classes=[permissions.IsAdminUser]
+class MenuItemSearchView(APIView):
+    def get(self,request):
+        query = request.GET.get("q","")
+        items = MenuItem.objects.filter(name__icontains= query)if query else MenuItem.objects.all()
 
-    def put(self, request, pk):
-        menu_item = get_object_or_404(MenuItem, pk=pk)
-        serializers = MenuItemSerializer(menu_item,data=request.data,partial=True)
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializer.data,status=status.HTTP_200_OK)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        page_number = request.GET.get("page",1)
+        paginator = paginator(items, 10)
+        page_obj = paginator.get_page(page_number)
+
+        serializer = MenuItemSerializer(page_obj, many=True)
+
+        return Response({
+            "count": paginator.count,
+            "total_pages":paginator.num_pages,
+            "current_page":page_obj.number,
+            "results":serializer.data,
+        } status=status.HTTP_200_OK)
         
