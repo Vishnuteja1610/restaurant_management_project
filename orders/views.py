86f2912f170e52ef574ec6758dc7f8557df82233
Nalumachu Vishnu Teja <vishnuteja2583@gmail.com>
2025-09-28 19:56:45 +0530

    51945

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status 
+from .models import Order
 
-# Create your views here.
+@api_view(['GET'])
+def order_status_view(request, order_id):
+    try:
+        order = Order.objects.get(id=order_id)
+        return Response({'order_id':order_id, 'status':order.status}, status=status.HTTP_200_OK)
+    except Order.DoesNotExist:
+        return Response({'error':'Order not found'}, status=status.HTTP_404_NOT_FOUND)
