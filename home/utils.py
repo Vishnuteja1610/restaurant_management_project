43a54b3632aa768b5f45c9e6b6c0f603f4390e8f
Nalumachu Vishnu Teja <vishnuteja2583@gmail.com>
2025-10-02 20:59:54 +0530

    53002

---
+++ home/utils.py
@@ -0,0 +1,11 @@
+from datetime import datetime, time
+
+def is_restaurant_open():
+    now = datetime.now()
+    weekday = now.weekday()
+
+    open_time = time(9,0)
+    close_time = time(22,0)
+
+    current_time = now.time()
+    return open_time <= current_time <=close_time
