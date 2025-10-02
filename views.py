6ecec5eda43c8637c46fbd6a644c467e07897cb3
Nalumachu Vishnu Teja <vishnuteja2583@gmail.com>
2025-10-02 20:57:01 +0530

    53003

--- views.py
+++ views.py
@@ -1,11 +1,8 @@
-from rest_framework.views import APIView
-from rest_framework.response import response
-from rest_framework import status
-from .models import Coupon
-from django.utils import timezone
+from rest_framework import generics 
+from .models import Table
+from .serializers import TableSerializer
 
-class CouponValidationview(APIView):
-    def post(self, request):
-        code = request.data.get('code')
-        if not code:
-            return response({'error': 'code is required' },status=status.HTTP_400_BAD_REQUEST)
+class TableDetailAPIView(generics.RetrieveAPIView):
+    queryset = Table.objects.all()
+    serializer_class = TableSerializer
+    
