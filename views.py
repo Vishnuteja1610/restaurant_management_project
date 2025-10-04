20bace5dd0a73a5595032e2033628e686fc940b6
Nalumachu Vishnu Teja <vishnuteja2583@gmail.com>
2025-10-04 21:56:20 +0530

    53493

--- views.py
+++ views.py
@@ -1,7 +1,11 @@
-from rest_framework import viewsets
-from .models import MenuCategory
-from .serializers import MenuCategorySerializer
+from rest_framework import generics
+from .models import Restaurant
+from .serializers import RestaurantSerializer
 
-class MenuCategoryViewSet(viewsets.ModelViewSet):
-    querryset = MenuCategory.objects.all()
-    serializer_class = MenuCategorySerializer
+class RestaurantInfoView(generics.RetrieveAPIView):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+
+    def get_object(self):
+        return Restaurant.objects.first()
+        
