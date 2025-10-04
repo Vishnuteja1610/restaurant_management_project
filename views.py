5823fa2adb61e82a51e3a717018ee49b82f714cd
Nalumachu Vishnu Teja <vishnuteja2583@gmail.com>
2025-10-04 21:25:04 +0530

    53496

--- views.py
+++ views.py
@@ -1,8 +1,7 @@
-from rest_framework import generics 
-from .models import Table
-from .serializers import TableSerializer
+from rest_framework import viewsets
+from .models import MenuCategory
+from .serializers import MenuCategorySerializer
 
-class TableDetailAPIView(generics.RetrieveAPIView):
-    queryset = Table.objects.all()
-    serializer_class = TableSerializer
-    
+class MenuCategoryViewSet(viewsets.ModelViewSet):
+    querryset = MenuCategory.objects.all()
+    serializer_class = MenuCategorySerializer
